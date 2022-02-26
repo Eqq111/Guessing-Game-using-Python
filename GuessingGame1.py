6ec0fc470afcb72f75843fd47c8b85f0add2ef68
Earl Marquise <95518992+Eqq111@users.noreply.github.com>
2022-02-26 11:02:58 -0500

    Update GuessingGame1.py

--- GuessingGame1.py
+++ GuessingGame1.py
@@ -1,6 +1,29 @@
+import random
 
+def computerGuess (lowval, highval, randnum, count=0):
+    if highval>= lowval:
+        guess = lowval + (highval - lowval) // 2
+        # If guess is in the middle. It is found!
+        if guess == randnum:
+           return count
 
+        #If "guess" is greater than the number,
+        #It must be found in the lower half of the set of numbers
+        #between the lower value and the guess
+        elif guess >randnum:
+            count = count + 1
+            return computerGuess(lowval, guess-1, randnum, count)
 
+        #the number must be in the upper set of numbers
+        #between the guess and the upper value
+        else:
+             count = count + 1
+             return computerGuess(guess + 1, highval, randnum, count)
+    else:
+        #Number not found
+        return -1
+    #end of function
+                
 #generate a random number between 1 and 100
 randnum = random.randint(1,101)
 
@@ -8,16 +31,16 @@ count = 0
 guess = -99
 
 while (guess != randnum):
-    #Get the user's guess
-    guess = (
-    if guess < randnum:
-            print ("higher")
-    else guess > randnum:
-            print ("lower")
-    else:
-            print ("you guessed it!")
-            break
-    count = count +1
+        #Get the user's guess
+        guess = (int)(input("Enter your guess between 1 and 100:"))
+        if guess < randnum:
+          print ("higher")
+        elif guess > randnum:
+          print ("lower")
+        else:
+          print ("you guessed it!")
+          break
+        count = count +1
 #end of while loop
 
-print("You took " + str(count) + "Ssteps to guess the number")
+print("You took " + str(count) + "steps to guess the number")
