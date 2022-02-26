82322d878b94895703223d9aff9554002617b679
Earl Marquise <95518992+Eqq111@users.noreply.github.com>
2022-02-26 10:33:28 -0500

    Update Guessing Gate pt2

--- GuessingGame1.py
+++ GuessingGame1.py
@@ -1,12 +1,21 @@
-#Get the user's guess
-guess = (int) (input("Enter your guess between 1 and 100:"))
+
+
 
 #generate a random number between 1 and 100
 randnum = random.randint(1,101)
 
-if guess < randnum:
-  print ("higher")
-else guess > randnum:
-  print ("lower")
-else:
-  print ("you guessed it!")
+count = 0
+guess = -99
+
+while (guess != randnum):
+    #Get the user's guess
+    guess = (
+    if guess < randnum:
+            print ("higher")
+    else guess > randnum:
+            print ("lower")
+    else:
+            print ("you guessed it!")
+            break
+    count = count +1
+#end of while loop
